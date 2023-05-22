873e14fbc75c7c1959c389196d7cfb83914ff68e
Dzakwan Dhiya Ulhaq <dzakwandhiya@student.telkomuniversity.ac.id>
2023-05-22 23:02:05 +0700

    add - buttom navigation

--- app/src/main/java/com/org/proyektingkat2/siagabanjir/MainActivity.kt
+++ app/src/main/java/com/org/proyektingkat2/siagabanjir/MainActivity.kt
@@ -2,10 +2,33 @@ package com.org.proyektingkat2.siagabanjir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.org.proyektingkat2.siagabanjir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//    navController=Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+//    setupWithNavController(binding.bottomNavigationView,navController)
+
+
     }
 }
+
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
